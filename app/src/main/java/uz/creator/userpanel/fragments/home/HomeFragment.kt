b4948395ba1fa1c24eb8e84b42be7258695693f9
Kozimjon <kozimjon0597@gmail.com>
2021-08-21 21:31:00 +0500

    Asrorjon

--- app/src/main/java/uz/creator/userpanel/fragments/home/HomeFragment.kt
+++ app/src/main/java/uz/creator/userpanel/fragments/home/HomeFragment.kt
@@ -1,5 +1,4 @@
 package uz.creator.userpanel.fragments.home
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import uz.creator.adminpanel.R
 import uz.creator.adminpanel.databinding.FragmentHomeBinding
-
-
 class HomeFragment : Fragment() {
    private  var _binding:FragmentHomeBinding?=null
     val binding get() =_binding!!
