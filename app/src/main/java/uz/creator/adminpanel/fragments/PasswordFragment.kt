c7cd3535021ab7312fe5d6b3516b3fd2424aec7f
Nitro5 <iommeomdev@gmail.com>
2021-08-21 14:35:53 +0500

    Set Views

--- app/src/main/java/uz/creator/adminpanel/fragments/PasswordFragment.kt
+++ app/src/main/java/uz/creator/adminpanel/fragments/PasswordFragment.kt
@@ -5,28 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import uz.creator.adminpanel.R
+import com.andrognito.pinlockview.PinLockListener
+import uz.creator.adminpanel.databinding.FragmentPasswordBinding
+import uz.creator.adminpanel.utils.showToast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class PasswordFragment : Fragment(), PinLockListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PasswordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PasswordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentPasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -34,27 +23,24 @@ class PasswordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password, container, false)
+        binding = FragmentPasswordBinding.inflate(layoutInflater)
+        binding.apply {
+            pinLockView.attachIndicatorDots(indicatorDots)
+            pinLockView.setPinLockListener(this@PasswordFragment)
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PasswordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PasswordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onComplete(pin: String?) {
+        requireContext().showToast(pin ?: "")
+        binding.pinLockView.resetPinLockView()
+    }
+
+    override fun onEmpty() {
+
+    }
+
+    override fun onPinChange(pinLength: Int, intermediatePin: String?) {
+
     }
 }
